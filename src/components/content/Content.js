3537eac336ac214c54588114f9d39c5510a0d957
Melvin Almonte <melvinalmonte@gmail.com>
2020-06-29 22:54:02 -0400

    mobile compatibility

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Content = ({ children }) => {
   return (
-    <section className="hero">
+    <section className="hero simple-cov-tile">
       <div className="hero-body">
         <div className="container">
-          <div className="tile is-ancestor simple-cov-tile">{children}</div>
+          <div className="tile is-ancestor">{children}</div>
         </div>
       </div>
     </section>
